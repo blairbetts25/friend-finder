07528b584d30cc2f9de3b3fc3f0fe36d768d810f
Blair Betts <blairbetts25@gmail.com>
2019-05-08 18:33:08 -0400

    finished hw

---
+++ app/routes/htmlroutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+var express = require("express");
+
+module.exports = function (app) {
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    app.use(express.static(path.join(__dirname, "../public")));
+};
